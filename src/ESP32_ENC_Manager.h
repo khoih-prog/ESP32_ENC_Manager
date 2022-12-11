9dfa0c4c6d488d175866cf9917e6b1cec4ea7220
Khoi Hoang <57012152+khoih-prog@users.noreply.github.com>
2022-12-11 01:30:05 -0500

    v1.0.0 for ESP32 + LwIP ENC28J60
    #### Releases v1.0.0
    
    1. Initial coding to port [ESP_WiFiManager](https://github.com/khoih-prog/ESP_WiFiManager) to ESP32 boards using `LwIP ENC28J60 Ethernet`.
    2. Use `allman astyle`

---
+++ src/ESP32_ENC_Manager.h
@@ -0,0 +1,32 @@
+/****************************************************************************************************************************
+  ESP32_ENC_Manager.h
+
+  For Ethernet shields using ESP32_ENC (ESP32 + LwIP ENC28J60)
+
+  WebServer_ESP32_ENC is a library for the ESP32 with Ethernet ENC28J60 to run WebServer
+
+  Modified from
+  1. Tzapu               (https://github.com/tzapu/WiFiManager)
+  2. Ken Taylor          (https://github.com/kentaylor)
+  3. Khoi Hoang          (https://github.com/khoih-prog/ESP_WiFiManager)
+
+  Built by Khoi Hoang https://github.com/khoih-prog/ESP32_ENC_Manager
+  Licensed under MIT license
+
+  Version: 1.0.0
+
+  Version Modified By  Date      Comments
+  ------- -----------  ---------- -----------
+  1.0.0   K Hoang     11/12/2022 Initial coding for ESP32_ENC
+ *****************************************************************************************************************************/
+
+#pragma once
+
+#ifndef ESP32_ENC_Manager_h
+#define ESP32_ENC_Manager_h
+
+#include <ESP32_ENC_Manager.hpp>              //https://github.com/khoih-prog/ESP32_ENC_Manager
+#include <ESP32_ENC_Manager_Impl.h>           //https://github.com/khoih-prog/ESP32_ENC_Manager
+
+#endif    // ESP32_ENC_Manager_h
+
